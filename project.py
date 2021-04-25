69c85960d88abc51607a64581e226847b74b8482
DJ_DELAIT <louis.delait@student.uclouvain.be>
2021-04-25 15:32:31 +0200

    OFDM beginning

--- project.py
+++ project.py
@@ -1,1 +1,79 @@
 proute = 1
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#######################TRANSMISSION CARACTERISTICS##########################
+
+K = 64 # number of OFDM subcarriers
+CP = K//4  # length of the cyclic prefix: 25% of the block
+P = 8 # number of pilot carriers per OFDM block
+pilotValue = 3+3j # The known value each pilot transmits
+
+allCarriers = np.arange(K)  # indices of all subcarriers ([0, 1, ... K-1])
+
+pilotCarriers = allCarriers[::K//P] # Pilots is every (K/P)th carrier.
+
+# For convenience of channel estimation, let's make the last carriers also be a pilot
+pilotCarriers = np.hstack([pilotCarriers, np.array([allCarriers[-1]])])
+P = P+1
+
+# data carriers are all remaining carriers
+dataCarriers = np.delete(allCarriers, pilotCarriers)
+
+print ("allCarriers:   %s" % allCarriers)
+print ("pilotCarriers: %s" % pilotCarriers)
+print ("dataCarriers:  %s" % dataCarriers)
+# plt.plot(pilotCarriers, np.zeros_like(pilotCarriers), 'bo', label='pilot')
+# plt.plot(dataCarriers, np.zeros_like(dataCarriers), 'ro', label='data')
+
+
+mu = 4 # bits per symbol (i.e. 16QAM)
+payloadBits_per_OFDM = len(dataCarriers)*mu  # number of payload bits per OFDM symbol
+
+
+
+
+#######################CONSTELLATION##########################
+
+
+
+mapping_table = {
+    (0,0,0,0) : -3-3j,
+    (0,0,0,1) : -3-1j,
+    (0,0,1,0) : -3+3j,
+    (0,0,1,1) : -3+1j,
+    (0,1,0,0) : -1-3j,
+    (0,1,0,1) : -1-1j,
+    (0,1,1,0) : -1+3j,
+    (0,1,1,1) : -1+1j,
+    (1,0,0,0) :  3-3j,
+    (1,0,0,1) :  3-1j,
+    (1,0,1,0) :  3+3j,
+    (1,0,1,1) :  3+1j,
+    (1,1,0,0) :  1-3j,
+    (1,1,0,1) :  1-1j,
+    (1,1,1,0) :  1+3j,
+    (1,1,1,1) :  1+1j
+}
+for b3 in [0, 1]:
+    for b2 in [0, 1]:
+        for b1 in [0, 1]:
+            for b0 in [0, 1]:
+                B = (b3, b2, b1, b0)
+                Q = mapping_table[B]
+                plt.plot(Q.real, Q.imag, 'bo')
+                plt.text(Q.real, Q.imag+0.2, "".join(str(x) for x in B), ha='center')
+                
+                
+                
+#######################CHANNEL RESPONSE##########################
+                
+channelResponse = np.array([1, 0, 0.3+0.3j])  # the impulse response of the wireless channel
+H_exact = np.fft.fft(channelResponse, K)
+plt.plot(allCarriers, abs(H_exact))
+
+SNRdb = 25  # signal to noise-ratio in dB at the receiver 
+plt.show()                
+print("proute")
